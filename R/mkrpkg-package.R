62cea77c84431ffce3d06caeb42902958fb657e8
Noam Ross <noam.ross@gmail.com>
2018-05-25 18:14:36 -0400

    fix package doc

--- R/mkrpkg-package.R
+++ R/mkrpkg-package.R
@@ -7,7 +7,8 @@
 #' The development repository for the **{{<<PACKAGE_NAME>>}}** package is found at
 #' \url{https://github.com/{{<<GH_USER>>}}/{{<<PACKAGE_NAME>>}}}.  Please file
 #' bug reports or other feedback at \url{https://github.com/{{<<GH_USER>>}}/{{<<PACKAGE_NAME>>}}/issues}
-
+#'
+#' @name {{<<PACKAGE_NAME>>}}
 #' @author {{<<AUTHOR>>}} \email{{<<AUTHOR_EMAIL>>}}
 #' @keywords package
 NULL
